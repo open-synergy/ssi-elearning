1eaafba8f19cfb6e53461d09d9f79af22cb321b6
Miftahussalam <miftah_salam@ymail.com>
2023-06-25 22:34:27 +0700

    [ssi_elearning] 14.0.1.0.0

---
+++ ssi_elearning/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2023 OpenSynergy Indonesia
+# Copyright 2023 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "E-Learning Extension",
+    "version": "14.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "license": "LGPL-3",
+    "installable": True,
+    "depends": [
+        "website_slides",
+    ],
+    "data": [
+        "views/website_slides_templates_course.xml",
+    ],
+    "demo": [],
+}
