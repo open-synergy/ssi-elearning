a725dd00466b539e1c9876cbbbf76a4d787a9bea
usa8bit <usa8bit@gmail.com>
2023-10-25 13:17:08 +0700

    [ssi_elearning_project] 14.0.1.0.0

---
+++ ssi_elearning_project/models/slide_channel.py
@@ -0,0 +1,21 @@
+# Copyright 2023 OpenSynergy Indonesia
+# Copyright 2023 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl-3.0-standalone.html).
+
+from odoo import fields, models
+
+
+class SlideChannel(models.Model):
+    _inherit = [
+        "slide.channel",
+        "mixin.task",
+    ]
+    _task_create_page = True
+    _task_page_xpath = "//page[1]"
+    _task_template_position = "after"
+
+    task_ids = fields.Many2many(
+        relation="rel_lead_2_task",
+        column1="channel_id",
+        column2="task_id",
+    )
