59a32e2d347d3071bffb5f18090fb3c7d31d8faa
mikevhe18 <viriyananda.michael@gmail.com>
2023-11-25 17:41:03 +0700

    [QC]

--- ssi_elearning_project/models/slide_channel.py
+++ ssi_elearning_project/models/slide_channel.py
@@ -2,7 +2,7 @@
 # Copyright 2023 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl-3.0-standalone.html).
 
-from odoo import fields, models
+from odoo import models
 
 
 class SlideChannel(models.Model):
@@ -13,9 +13,3 @@ class SlideChannel(models.Model):
     _task_create_page = True
     _task_page_xpath = "//page[1]"
     _task_template_position = "after"
-
-    task_ids = fields.Many2many(
-        relation="rel_lead_2_task",
-        column1="channel_id",
-        column2="task_id",
-    )
